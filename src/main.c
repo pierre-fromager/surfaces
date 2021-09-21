feefd480df951c05a73a1ac89b6f4b6858acaf59
Pierre Fromager <info@pier-infor.fr>
2021-09-21 14:55:12 +0200

    ref : separate intervals cases single and multiple

--- src/main.c
+++ src/main.c
@@ -22,36 +22,57 @@
 #define IVL_M 2.0f
 #define IVL_H 5.0f
 
-static void demo_trapezoid(void);
+arguments_t args;
 
-static void demo_trapezoid()
+static interval_t single_interval(void);
+static void setup_itvls(intervals_t itvls, interval_t itvl_tpl);
+static double trapezoid_s_interval(linear_fn_t f, interval_t interval);
+static double trapezoid_m_intervals(linear_fn_t f, intervals_t itvls, unsigned nbintvls);
+
+static interval_t single_interval()
 {
-    linear_fn_t f0 = {.s = FN0_S, .o = FN0_O};
-    interval_t interval = {.l = IVL_L, .h = IVL_H};
-    const double sol_ivl = integ_trapezoid(f0, interval);
-    print_sol(stdout, f0, interval, sol_ivl);
+    return (interval_t){.l = IVL_L, .h = IVL_H};
+}
 
-    const unsigned nbitvls = 2;
-    intervals_t itvls = malloc(sizeof(interval_t) * nbitvls);
-    itvls[0] = interval;
-    itvls[0].h = IVL_M;
-    itvls[1] = interval;
-    itvls[1].l =itvls[0].h;
-    double sol_itvls0 = integ_trapezoid(f0, itvls[0]);
-    print_sol(stdout, f0, itvls[0], sol_itvls0);
-    double sol_itvls1 = integ_trapezoid(f0, itvls[1]);
-    print_sol(stdout, f0, itvls[1], sol_itvls1);
+static void setup_itvls(intervals_t itvls, interval_t itvl_tpl)
+{
+    itvls[0] = itvls[1] = itvl_tpl;
+    itvls[0].h = itvls[1].l = IVL_M;
+}
 
-    print_sol(stdout, f0, interval, sol_itvls0 + sol_itvls1);
-    free(itvls);
+static double trapezoid_s_interval(linear_fn_t f, interval_t itvl)
+{
+    return integ_trapezoid(f, itvl);
+}
 
-    assert(sol_ivl == (sol_itvls0 + sol_itvls1));
+static double trapezoid_m_intervals(linear_fn_t f, intervals_t itvls, unsigned nbintvls)
+{
+    unsigned itvlcpt;
+    double sol, solsum = 0;
+    for (itvlcpt = 0; itvlcpt < nbintvls; itvlcpt++)
+    {
+        sol = integ_trapezoid(f, itvls[itvlcpt]);
+        solsum += sol;
+        print_sol(stdout, f, itvls[itvlcpt], sol);
+    }
+    return solsum;
 }
 
 int main(int argc, char *argv[])
 {
-    arguments_t args;
     arguments_process(argc, argv, &args);
-    demo_trapezoid();
+
+    linear_fn_t f = {.s = FN0_S, .o = FN0_O};
+    const interval_t itvl_tpl = single_interval();
+    const double sol_s = trapezoid_s_interval(f, itvl_tpl);
+    print_sol(stdout, f, itvl_tpl, sol_s);
+
+    const unsigned nbitvls = 2;
+    intervals_t itvls = malloc(sizeof(interval_t) * nbitvls);
+    setup_itvls(itvls, itvl_tpl);
+    const double sol_m = trapezoid_m_intervals(f, itvls, nbitvls);
+    free(itvls);
+    
+    assert(sol_s == sol_m);
     return EXIT_SUCCESS;
 }
