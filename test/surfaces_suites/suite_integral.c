00ebcc8a636b9db2d70132cd3ea418aebbd3f638
Pierre Fromager <info@pier-infor.fr>
2021-10-06 09:52:44 +0200

    test : riemann sums

--- test/surfaces_suites/suite_integral.c
+++ test/surfaces_suites/suite_integral.c
@@ -29,13 +29,17 @@ static struct
 } test_functions[] = {
     {test_surfaces_integral_trapez_o0, "integral_trapez_o0"},
     {test_surfaces_integral_midpnt_o0, "integral_midpnt_o0"},
-    {test_surfaces_integral_riemann_o0, "integral_riemann_o0"},
+    {test_surfaces_integral_riemann_left_o0, "integral_riemann_left_o0"},
+    {test_surfaces_integral_riemann_right_o0, "integral_riemann_right_o0"},
+    {test_surfaces_integral_riemann_trap_o0, "integral_riemann_trap_o0"},
+    {test_surfaces_integral_riemann_rect_o0, "integral_riemann_rect_o0"},
+    {test_surfaces_integral_riemann_mp_o0, "integral_riemann_mp_o0"},
     {test_surfaces_integral_simpson_o0, "integral_simpson_o0"},
     {test_surfaces_integral_factory_o0, "integral_factory_o0"},
     {test_surfaces_integral_trapez_o1, "integral_trapez_o1"},
     {test_surfaces_integral_midpnt_o1, "integral_midpnt_o1"},
-    {test_surfaces_integral_riemann_o1, "integral_riemann_o1"},
-
+    {test_surfaces_integral_riemann_left_o1, "integral_riemann_left_o1"},
+    {test_surfaces_integral_riemann_right_o1, "integral_riemann_right_o1"},
     {test_surfaces_integral_factory_o1, "integral_factory_o1"},
     /*{test_surfaces_integral_o1, "integral_o1"},
     {test_surfaces_integral_o2, "integral_o2"},
@@ -113,27 +117,134 @@ void test_surfaces_integral_midpnt_o0()
     }
 }
 
-void test_surfaces_integral_riemann_o0()
+void test_surfaces_integral_riemann_left_o0()
 {
     const polynomial_order_t io = 0;
     const polynomial_item_t ev = 100.0f;
+    const polynomial_item_t nb_rect = 2.0f;
     polynomial_order_t iocpt;
-    polynomial_item_t cptv;
     CU_ASSERT_PTR_NOT_NULL_FATAL(pol);
     polynomial_construct(io, pol);
     for (iocpt = 0; iocpt < io + 1; iocpt++)
         CU_ASSERT_EQUAL(polynomial_getorder(iocpt, pol), iocpt);
     for (iocpt = 0; iocpt < io + 1; iocpt++)
         CU_ASSERT_EQUAL(polynomial_getfactor(iocpt, pol), io);
-    // y = 10
+    // y = ev
     polynomial_setfactor(0, ev, pol);
     CU_ASSERT_EQUAL(polynomial_getfactor(0, pol), ev);
-    // y = x => (-ev < x < ev)
-    for (cptv = -ev; cptv < ev; cptv++)
-    {
-        polynomial_setfactor(io, cptv, pol);
-        //CU_ASSERT_EQUAL(integral_poly_riemann(pol, iv, 1.0f), (iv.h - iv.l) * cptv);
-    }
+    const polynomial_item_t ifr_left = integral_factory_riemann(
+        pol,
+        iv,
+        nb_rect,
+        riemann_left);
+    CU_ASSERT_DOUBLE_EQUAL(
+        (double)ifr_left,
+        (double)(5.0f * ev),
+        0.00001);
+}
+
+void test_surfaces_integral_riemann_right_o0()
+{
+    const polynomial_order_t io = 0;
+    const polynomial_item_t ev = 100.0f;
+    const polynomial_item_t nb_rect = 2.0f;
+    polynomial_order_t iocpt;
+    CU_ASSERT_PTR_NOT_NULL_FATAL(pol);
+    polynomial_construct(io, pol);
+    for (iocpt = 0; iocpt < io + 1; iocpt++)
+        CU_ASSERT_EQUAL(polynomial_getorder(iocpt, pol), iocpt);
+    for (iocpt = 0; iocpt < io + 1; iocpt++)
+        CU_ASSERT_EQUAL(polynomial_getfactor(iocpt, pol), io);
+    // y = ev
+    polynomial_setfactor(0, ev, pol);
+    CU_ASSERT_EQUAL(polynomial_getfactor(0, pol), ev);
+    const polynomial_item_t ifr_right = integral_factory_riemann(
+        pol,
+        iv,
+        nb_rect,
+        riemann_right);
+    CU_ASSERT_DOUBLE_EQUAL(
+        (double)ifr_right,
+        (double)(5.0f * ev),
+        0.00001);
+}
+
+void test_surfaces_integral_riemann_trap_o0()
+{
+    const polynomial_order_t io = 0;
+    const polynomial_item_t ev = 100.0f;
+    const polynomial_item_t nb_rect = 2.0f;
+    polynomial_order_t iocpt;
+    CU_ASSERT_PTR_NOT_NULL_FATAL(pol);
+    polynomial_construct(io, pol);
+    for (iocpt = 0; iocpt < io + 1; iocpt++)
+        CU_ASSERT_EQUAL(polynomial_getorder(iocpt, pol), iocpt);
+    for (iocpt = 0; iocpt < io + 1; iocpt++)
+        CU_ASSERT_EQUAL(polynomial_getfactor(iocpt, pol), io);
+    // y = ev
+    polynomial_setfactor(0, ev, pol);
+    CU_ASSERT_EQUAL(polynomial_getfactor(0, pol), ev);
+    const polynomial_item_t ifr_trap = integral_factory_riemann(
+        pol,
+        iv,
+        nb_rect,
+        riemann_trapezoid);
+    CU_ASSERT_DOUBLE_EQUAL(
+        (double)ifr_trap,
+        (double)(5.0f * ev),
+        0.00001);
+}
+
+void test_surfaces_integral_riemann_rect_o0()
+{
+    const polynomial_order_t io = 0;
+    const polynomial_item_t ev = 100.0f;
+    const polynomial_item_t nb_rect = 2.0f;
+    polynomial_order_t iocpt;
+    CU_ASSERT_PTR_NOT_NULL_FATAL(pol);
+    polynomial_construct(io, pol);
+    for (iocpt = 0; iocpt < io + 1; iocpt++)
+        CU_ASSERT_EQUAL(polynomial_getorder(iocpt, pol), iocpt);
+    for (iocpt = 0; iocpt < io + 1; iocpt++)
+        CU_ASSERT_EQUAL(polynomial_getfactor(iocpt, pol), io);
+    // y = ev
+    polynomial_setfactor(0, ev, pol);
+    CU_ASSERT_EQUAL(polynomial_getfactor(0, pol), ev);
+    const polynomial_item_t ifr_rect = integral_factory_riemann(
+        pol,
+        iv,
+        nb_rect,
+        riemann_rectangle);
+    CU_ASSERT_DOUBLE_EQUAL(
+        (double)ifr_rect,
+        (double)(5.0f * ev),
+        0.00001);
+}
+
+void test_surfaces_integral_riemann_mp_o0()
+{
+    const polynomial_order_t io = 0;
+    const polynomial_item_t ev = 100.0f;
+    const polynomial_item_t nb_rect = 2.0f;
+    polynomial_order_t iocpt;
+    CU_ASSERT_PTR_NOT_NULL_FATAL(pol);
+    polynomial_construct(io, pol);
+    for (iocpt = 0; iocpt < io + 1; iocpt++)
+        CU_ASSERT_EQUAL(polynomial_getorder(iocpt, pol), iocpt);
+    for (iocpt = 0; iocpt < io + 1; iocpt++)
+        CU_ASSERT_EQUAL(polynomial_getfactor(iocpt, pol), io);
+    // y = ev
+    polynomial_setfactor(0, ev, pol);
+    CU_ASSERT_EQUAL(polynomial_getfactor(0, pol), ev);
+    const polynomial_item_t ifr_mp = integral_factory_riemann(
+        pol,
+        iv,
+        nb_rect,
+        riemann_middle_point);
+    CU_ASSERT_DOUBLE_EQUAL(
+        (double)ifr_mp,
+        (double)(5.0f * ev),
+        0.00001);
 }
 
 void test_surfaces_integral_simpson_o0()
@@ -148,7 +259,7 @@ void test_surfaces_integral_simpson_o0()
         CU_ASSERT_EQUAL(polynomial_getorder(iocpt, pol), iocpt);
     for (iocpt = 0; iocpt < io + 1; iocpt++)
         CU_ASSERT_EQUAL(polynomial_getfactor(iocpt, pol), io);
-    // y = 10
+    // y = ev
     polynomial_setfactor(0, ev, pol);
     CU_ASSERT_EQUAL(polynomial_getfactor(0, pol), ev);
     // y = x => (-ev < x < ev)
@@ -171,7 +282,7 @@ void test_surfaces_integral_factory_o0()
         CU_ASSERT_EQUAL(polynomial_getorder(iocpt, pol), iocpt);
     for (iocpt = 0; iocpt < io + 1; iocpt++)
         CU_ASSERT_EQUAL(polynomial_getfactor(iocpt, pol), io);
-    // y = 10
+    // y = 100
     polynomial_setfactor(0, ev, pol);
     CU_ASSERT_EQUAL(polynomial_getfactor(0, pol), ev);
     // y = x => (-ev < x < ev)
@@ -254,13 +365,10 @@ void test_surfaces_integral_midpnt_o1()
     CU_ASSERT_EQUAL(integral_poly_midpnt(pol, iv), expected2xplusten);
 }
 
-void test_surfaces_integral_riemann_o1()
+void test_surfaces_integral_riemann_left_o1()
 {
     const polynomial_order_t io = 1;
     const polynomial_item_t ev = 100.0f;
-    const polynomial_item_t expected = 12.5f;
-    const polynomial_item_t expectedplusten = 62.5f;
-    const polynomial_item_t expected2xplusten = 74.997500f;
     polynomial_order_t iocpt;
     polynomial_item_t cptv;
     CU_ASSERT_PTR_NOT_NULL_FATAL(pol);
@@ -276,23 +384,68 @@ void test_surfaces_integral_riemann_o1()
     for (cptv = -ev; cptv < ev; cptv++)
     {
         polynomial_setfactor(1, cptv, pol);
-        CU_ASSERT_EQUAL(
+        CU_ASSERT_DOUBLE_EQUAL(
             integral_factory_riemann(pol, iv, 1.0f, riemann_left),
-            expected * cptv);
+            0.0f,
+            0.00001);
     }
     // y = x + 10
     polynomial_setfactor(0, 10.0f, pol);
     polynomial_setfactor(1, 1.0f, pol);
     CU_ASSERT_EQUAL(polynomial_getfactor(0, pol), 10.0f);
     CU_ASSERT_EQUAL(polynomial_getfactor(1, pol), 1.0f);
-    CU_ASSERT_EQUAL(integral_factory_riemann(pol, iv, 1.0f, riemann_left), expectedplusten);
+    CU_ASSERT_DOUBLE_EQUAL(
+        (double)integral_factory_riemann(pol, iv, 1.0f, riemann_left),
+        (double)50.0f,
+        0.00001);
+
     // y = 2x + 10
     polynomial_setfactor(1, 2.0f, pol);
     CU_ASSERT_EQUAL(polynomial_getfactor(1, pol), 2.0f);
-    printf(
-        "\nintegral_poly_riemann n 3 %Lf\n",
-        integral_factory_riemann(pol, iv, 3.0f, riemann_left));
-    CU_ASSERT_EQUAL(integral_factory_riemann(pol, iv, 3.0f, riemann_left), expected2xplusten);
+    CU_ASSERT_DOUBLE_EQUAL(
+        (double)integral_factory_riemann(pol, iv, 3.0f, riemann_left),
+        (double)66.666667f,
+        0.00001);
+}
+
+void test_surfaces_integral_riemann_right_o1()
+{
+    const polynomial_order_t io = 1;
+    const polynomial_item_t ev = 100.0f;
+    polynomial_order_t iocpt;
+    //polynomial_item_t cptv;
+    CU_ASSERT_PTR_NOT_NULL_FATAL(pol);
+    polynomial_construct(io, pol);
+    for (iocpt = 0; iocpt < io + 1; iocpt++)
+        CU_ASSERT_EQUAL(polynomial_getorder(iocpt, pol), iocpt);
+    for (iocpt = 0; iocpt < io + 1; iocpt++)
+        CU_ASSERT_EQUAL(polynomial_getfactor(iocpt, pol), 0);
+    // y = x
+    polynomial_setfactor(1, ev, pol);
+    CU_ASSERT_EQUAL(polynomial_getfactor(1, pol), ev);
+    // y = cptv => (-ev < cptv < ev)
+    printf("%Lf\n", integral_factory_riemann(pol, iv, 2.0f, riemann_right));
+    CU_ASSERT_DOUBLE_EQUAL(
+        (double)integral_factory_riemann(pol, iv, 2.0f, riemann_right),
+        (double)1875.00f,
+        0.00001);
+    // y = x + 10
+    polynomial_setfactor(0, 10.0f, pol);
+    polynomial_setfactor(1, 1.0f, pol);
+    CU_ASSERT_EQUAL(polynomial_getfactor(0, pol), 10.0f);
+    CU_ASSERT_EQUAL(polynomial_getfactor(1, pol), 1.0f);
+    CU_ASSERT_DOUBLE_EQUAL(
+        (double)integral_factory_riemann(pol, iv, 2.0f, riemann_right),
+        (double)68.75f,
+        0.00001);
+
+    // y = 2x + 10
+    polynomial_setfactor(1, 2.0f, pol);
+    CU_ASSERT_EQUAL(polynomial_getfactor(1, pol), 2.0f);
+    CU_ASSERT_DOUBLE_EQUAL(
+        (double)integral_factory_riemann(pol, iv, 3.0f, riemann_right),
+        (double)83.333333f,
+        0.00001);
 }
 
 void test_surfaces_integral_factory_o1()
--- test/surfaces_suites/suite_integral.h
+++ test/surfaces_suites/suite_integral.h
@@ -18,13 +18,17 @@ void reset_test_integral(void);
 
 void test_surfaces_integral_trapez_o0(void);
 void test_surfaces_integral_midpnt_o0(void);
-void test_surfaces_integral_riemann_o0(void);
+void test_surfaces_integral_riemann_left_o0(void);
+void test_surfaces_integral_riemann_right_o0(void);
+void test_surfaces_integral_riemann_trap_o0(void);
+void test_surfaces_integral_riemann_rect_o0(void);
+void test_surfaces_integral_riemann_mp_o0(void);
 void test_surfaces_integral_simpson_o0(void);
 void test_surfaces_integral_factory_o0(void);
-
 void test_surfaces_integral_trapez_o1(void);
 void test_surfaces_integral_midpnt_o1(void);
-void test_surfaces_integral_riemann_o1(void);
+void test_surfaces_integral_riemann_left_o1(void);
+void test_surfaces_integral_riemann_right_o1(void);
 void test_surfaces_integral_factory_o1(void);
 /*
 void test_surfaces_integral_o1(void);
